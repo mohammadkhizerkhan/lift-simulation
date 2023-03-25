dbf1e9bee51579ba891d79c6c6d4cbacdf3db4b0
mohammadkhizerkhan <mohammad.khizer@m2pfintech.com>
2023-03-25 06:23:38 +0530

    completed without doors

--- script.js
+++ script.js
@@ -1,64 +1,75 @@
+const floors = document.getElementById("floor");
+const lifts = document.getElementById("lift");
+const liftContainer = document.querySelector(".lifts-container");
+const floorsContainer = document.querySelector(".floors-container");
+const submitBtn = document.getElementById("submit");
+let noOfLifts = 0;
+let noOfFloors = 0;
 
-const floors=document.getElementById('floor')
-const lifts=document.getElementById('lift')
-const liftContainer=document.querySelector('.lifts-container');
-const floorsContainer=document.querySelector('.floors-container');
-const submitBtn=document.getElementById('submit')
-let noOfLifts=0;
-let noOfFloors=0;
+floors.addEventListener("change", (e) => {
+  noOfFloors = e.target.value;
+});
 
-floors.addEventListener('change',(e)=>{
-    noOfFloors=e.target.value;
-})
+lifts.addEventListener("change", (e) => {
+  noOfLifts = e.target.value;
+});
 
-lifts.addEventListener('change',(e)=>{
-    noOfLifts=e.target.value;
-})
+const moveLift = (position) => {
+  const lifts = document.querySelectorAll(".lift");
+  const distance = 165 * (position - 1);
+  lifts[0].style.transform = `translateY(-${distance}px)`;
+  lifts[0].style.transition = `all ${2}s linear`;
+};
 
-const createLifts=()=>{
-    for(let i=0;i<Number(noOfLifts);i++){
-        const liftDiv=document.createElement("div");
-        liftDiv.classList.add('lift');
-        liftContainer.appendChild(liftDiv)
-    }
-}
+const handleFloorBtn = (floorNum) => {
+  console.log("=======", floorNum);
+  moveLift(floorNum);
+};
 
-const createFloors=()=>{
-    for(let i=Number(noOfFloors);i>=0;i--){
+const createLifts = () => {
+  for (let i = 0; i < Number(noOfLifts); i++) {
+    const liftDiv = document.createElement("div");
+    liftDiv.classList.add("lift", `lift-${i + 1}`);
+    liftContainer.appendChild(liftDiv);
+  }
+};
 
-        const floorSection=document.createElement("section");
-        floorSection.classList.add('floor');
+const createFloors = () => {
+  for (let i = Number(noOfFloors); i >= 1; i--) {
+    const floorSection = document.createElement("section");
+    floorSection.classList.add("floor", `floor-${i}`);
 
-        const floorDetailsDiv=document.createElement("div");
-        floorDetailsDiv.classList.add('floor-details');
+    const floorDetailsDiv = document.createElement("div");
+    floorDetailsDiv.classList.add("floor-details");
 
-        const floorHeader=document.createElement("h3");
-        floorHeader.classList.add('floor-name');
-        floorHeader.innerText=`Floor ${i}`
-    
+    const floorHeader = document.createElement("h3");
+    floorHeader.classList.add("floor-name");
+    floorHeader.innerText = `Floor ${i}`;
 
-        const floorBtnUp=document.createElement("button");
-        floorBtnUp.classList.add('primary-btn');
-        floorBtnUp.innerText='up'
+    const floorBtnUp = document.createElement("button");
+    floorBtnUp.classList.add("primary-btn");
+    floorBtnUp.innerText = "up";
+    floorBtnUp.addEventListener("click", () => {
+      handleFloorBtn(i);
+    });
 
+    const floorBtnDown = document.createElement("button");
+    floorBtnDown.classList.add("primary-btn");
+    floorBtnDown.innerText = "down";
+    floorBtnDown.addEventListener("click", () => {
+      handleFloorBtn(i);
+    });
 
-        const floorBtnDown=document.createElement("button");
-        floorBtnDown.classList.add('primary-btn');
-        floorBtnDown.innerText='down'
+    console.log("-----", i === noOfFloors);
+    floorDetailsDiv.appendChild(floorHeader);
+    i !== Number(noOfFloors) && floorDetailsDiv.appendChild(floorBtnUp);
+    i !== 1 && floorDetailsDiv.appendChild(floorBtnDown);
+    floorSection.appendChild(floorDetailsDiv);
+    floorsContainer.appendChild(floorSection);
+  }
+};
 
-        floorDetailsDiv.appendChild(floorHeader)
-        floorDetailsDiv.appendChild(floorBtnUp)
-        floorDetailsDiv.appendChild(floorBtnDown)
-        floorSection.appendChild(floorDetailsDiv)
-        floorsContainer.appendChild(floorSection)
-    }
-}
-
-submitBtn.addEventListener('click',()=>{
-  createFloors()
-    createLifts();
-})
-
-
-
-
+submitBtn.addEventListener("click", () => {
+  createFloors();
+  createLifts();
+});
