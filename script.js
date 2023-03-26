45516b2d632020c6622451d4f32dae1841fb474d
mohammadkhizerkhan <mohammad.khizer@m2pfintech.com>
2023-03-26 23:19:01 +0530

    adding attributes to lift

--- script.js
+++ script.js
@@ -14,11 +14,30 @@ lifts.addEventListener("change", (e) => {
   noOfLifts = e.target.value;
 });
 
+const openCloseDoors = (lift, position) => {
+  const openTiming = 2000 * position;
+  const closeTiming = 2000 * position + 2500;
+  setTimeout(() => {
+    console.log("openint");
+    lift.childNodes[0].classList.add("left-door-open");
+    lift.childNodes[1].classList.add("right-door-open");
+    lift.setAttribute("data-lift-status", "free");
+  }, openTiming);
+  setTimeout(() => {
+    console.log("closing");
+    lift.childNodes[0].classList.remove("left-door-open");
+    lift.childNodes[1].classList.remove("right-door-open");
+  }, closeTiming);
+};
+
 const moveLift = (position) => {
   const lifts = document.querySelectorAll(".lift");
   const distance = 165 * (position - 1);
   lifts[0].style.transform = `translateY(-${distance}px)`;
-  lifts[0].style.transition = `all ${2}s linear`;
+  lifts[0].style.transition = `all ${2 * position}s linear`;
+  lifts[0].setAttribute("data-lift-status", "busy");
+  lifts[0].setAttribute("data-lift-position", `${position}`);
+  openCloseDoors(lifts[0], position);
 };
 
 const handleFloorBtn = (floorNum) => {
@@ -30,6 +49,17 @@ const createLifts = () => {
   for (let i = 0; i < Number(noOfLifts); i++) {
     const liftDiv = document.createElement("div");
     liftDiv.classList.add("lift", `lift-${i + 1}`);
+    liftDiv.setAttribute("data-lift-status", "free");
+    liftDiv.setAttribute("data-lift-position", `${i + 1}`);
+
+    const left_door = document.createElement("div");
+    left_door.classList.add("door", "left-door");
+
+    const right_door = document.createElement("div");
+    right_door.classList.add("door", "right-door");
+
+    liftDiv.appendChild(left_door);
+    liftDiv.appendChild(right_door);
     liftContainer.appendChild(liftDiv);
   }
 };
@@ -60,7 +90,6 @@ const createFloors = () => {
       handleFloorBtn(i);
     });
 
-    console.log("-----", i === noOfFloors);
     floorDetailsDiv.appendChild(floorHeader);
     i !== Number(noOfFloors) && floorDetailsDiv.appendChild(floorBtnUp);
     i !== 1 && floorDetailsDiv.appendChild(floorBtnDown);
